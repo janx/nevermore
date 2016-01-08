2acf2cca0386e4f09ca388ebc4a51dcd7b812356
Jan Xie <jan.h.xie@gmail.com>
2016-01-09 00:18:30 +0800

    submit record

--- contracts/CreditBook.sol
+++ contracts/CreditBook.sol
@@ -3,22 +3,28 @@ import 'mortal';
 
 contract CreditBook is owned, mortal {
 
-  struct RecordHeader {
-    bytes32 id;
+  struct Record {
+    bytes32 identity;
     uint16 category;
     uint16 state;
     uint256 fee;
     uint256 timestamp;
+    bytes32 commit;
   }
 
   address public owner;
 
-  mapping(bytes32 => RecordHeader) records;
+  Record[] public records;
+
+  event NewRecord(bytes32 indexed user, uint16 indexed category, uint16 indexed state, uint256 fee, uint256 timestamp, uint256 id);
 
   function CreditBook() {
     owner = msg.sender;
   }
 
-  function submit(bytes32 id, uint16 category, uint16 state, uint256 fee, uint256 timestamp) {
+  function submit(bytes32 user, uint16 category, uint16 state, uint256 fee, uint256 timestamp, bytes32 commit) external {
+    records.push(Record(user, category, state, fee, timestamp, commit));
+    NewRecord(user, category, state, fee, timestamp, records.length-1);
   }
+
 }
--- test/CreditBook.js
+++ test/CreditBook.js
@@ -8,4 +8,28 @@ contract('CreditBook', function(accounts) {
       done();
     }).catch(done);
   });
+
+  it("should submit new record", function(done) {
+    var book = CreditBook.deployed();
+
+    var event = book.NewRecord({});
+    event.watch(function(error, result) {
+      if(!error) {
+        assert.equal(user, result.args.user);
+        assert.equal('0', result.args.id.toString());
+        assert.equal('100', result.args.fee.toString());
+        assert.equal('2718281828', result.args.timestamp.toString());
+        done();
+      }
+    });
+
+    var user = '0x0000000000000000000000000000000000000000000000000000000012345678';
+    var category = 0;
+    var state = 0;
+    var fee = 100;
+    var timestamp = 2718281828;
+    var commit = 'abcdefg';
+    book.submit(user, category, state, fee, timestamp, commit);
+  });
+
 });
