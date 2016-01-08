4241aba325555cd7aa3dee36fedf7e0ab58fe33e
Terry Tai <poshboytl@gmail.com>
2016-01-08 23:37:39 +0800

    Try to comunicate with blockchain with web3.

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -2,3 +2,10 @@ console.log("jQuery loaded");
 console.log($);
 console.log("Angular loaded");
 console.log(angular);
+
+// Try to communicate with blockchain
+$(document).ready(function(){
+  var book = CreditBook.deployed();
+
+  var address = web3.eth.accounts[0]
+});
