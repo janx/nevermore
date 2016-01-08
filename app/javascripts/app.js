2cdd76146ab806ce70a7d03f93b772a2c627747f
Daniel Lv <lgn21st@gmail.com>
2016-01-08 23:40:58 +0800

    Bootstrap angular's module

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -2,3 +2,14 @@ console.log("jQuery loaded");
 console.log($);
 console.log("Angular loaded");
 console.log(angular);
+
+
+// Bootstrap Angualr module
+angular.module('Nevermore', [])
+  .controller('MyController', ['$scope', function ($scope) {
+    $scope.greetMe = 'World';
+  }]);
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['Nevermore']);
+});
