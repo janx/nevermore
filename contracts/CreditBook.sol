787453f6d87f03b0e640467dd2565c2df89e4e4e
Jan Xie <jan.h.xie@gmail.com>
2016-01-09 09:40:22 +0800

    make sure commit is unique

--- contracts/CreditBook.sol
+++ contracts/CreditBook.sol
@@ -15,7 +15,8 @@ contract CreditBook is owned, mortal {
 
   address public owner;
 
-  Record[] public records;
+  bytes32[] index;
+  mapping(bytes32 => Record) records;
 
   event NewRecord(bytes32 indexed user, uint16 indexed category, uint16 indexed state, uint256 fee, uint256 timestamp, address provider, uint256 id);
 
@@ -24,13 +25,18 @@ contract CreditBook is owned, mortal {
   }
 
   function size() constant returns (uint256) {
-    return records.length;
+    return index.length;
   }
 
-  // TODO: make sure commit is unique
   function submit(bytes32 user, uint16 category, uint16 state, uint256 fee, uint256 timestamp, bytes32 commit) external {
-    records.push(Record(msg.sender, user, category, state, fee, timestamp, commit));
-    NewRecord(user, category, state, fee, timestamp, msg.sender, records.length-1);
+    if(user == bytes32(0x0)) throw;
+    if(timestamp == 0) throw;
+    if(records[commit].timestamp != 0) throw; // commit must be unique
+
+    index.push(commit);
+    records[commit] = Record(msg.sender, user, category, state, fee, timestamp, commit);
+
+    NewRecord(user, category, state, fee, timestamp, msg.sender, index.length-1);
   }
 
 }
--- test/CreditBook.js
+++ test/CreditBook.js
@@ -4,7 +4,12 @@ contract('CreditBook', function(accounts) {
   var state = 0;
   var fee = 100;
   var timestamp = 2718281828;
-  var commit = 'abcdefg';
+
+  var commit = 0;
+  var genCommit = function() {
+    commit += 1;
+    return ''+commit;
+  };
 
   it("should set owner on contract creation", function(done) {
     var book = CreditBook.deployed();
@@ -19,8 +24,8 @@ contract('CreditBook', function(accounts) {
     var book = CreditBook.deployed();
 
     Promise.all([
-      book.submit(user, category, state, fee, timestamp, commit),
-      book.submit(user, category, state, fee, timestamp, commit)
+      book.submit(user, category, state, fee, timestamp, genCommit()),
+      book.submit(user, category, state, fee, timestamp, genCommit())
     ]).then(function() {
       return book.size();
     }).then(function(size) {
@@ -48,8 +53,46 @@ contract('CreditBook', function(accounts) {
           done();
         }).catch(done);
 
-      book.submit(user, category, state, fee, timestamp, commit);
+      book.submit(user, category, state, fee, timestamp, genCommit());
     }).catch(done);
+  });
+
+  it("should validate commit uniqueness", function(done) {
+    var book = CreditBook.deployed();
+
+    book.submit(user, category, state, fee, timestamp, 'usedcommit')
+      .then(function(txid) {
+        return book.submit(user, category, state, fee, timestamp, 'usedcommit');
+      }).then(function(txid) {
+        assert(!txid, "the second submit should not be succeed!");
+      }).catch(function(e) {
+        assert.match(e.message, /TransactionFailed/, "submit should fail");
+        done();
+      });
+  });
+
+  it("should validate user hash", function(done) {
+    var book = CreditBook.deployed();
+
+    book.submit(0, category, state, fee, timestamp, genCommit())
+      .then(function(txid) {
+        assert(!txid, "submit succeed!");
+      }).catch(function(e) {
+        assert.match(e.message, /TransactionFailed/, "submit should fail because user is zero");
+        done();
+      });
+  });
+
+  it("should validate timestamp", function(done) {
+    var book = CreditBook.deployed();
+
+    book.submit(user, category, state, fee, 0, genCommit())
+      .then(function(txid) {
+        assert(!txid, "submit succeed!");
+      }).catch(function(e) {
+        assert.match(e.message, /TransactionFailed/, "submit should fail because timestamp is zero");
+        done();
+      });
   });
 
 });
