350348dad9dbdc3096ba591597ced5cc278ac16a
Terry Tai <poshboytl@gmail.com>
2016-01-10 10:20:51 +0800

    Will this be the final commit?

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -563,19 +563,23 @@ angular.element(document).ready(function() {
 
   order_book.NewResponse({}, {address: OrderBook.deployed_address}, function(error, result) {
     var id = result.args.id.toNumber();
-    request = window.requests[id]
-    if(currentUser === request.from) {
-      $.publish('notice', 'Your request has been responsed.');
 
-      $.each(credit_records, function(index, value) {
-        if(value.commit === request.commit) {
-          value.orderstate = 2;
-        }
-      });
-    }
-    if(currentUser === request.provider) {
-      $.publish('notice', 'Your response has been sent automatically.');
-    }
+    order_book.getResponse(id).then(function(result) {
+      request = window.requests[id]
+      if(currentUser === request.from) {
+        $.publish('notice', 'Your request has been responsed.');
+
+        $.each(credit_records, function(index, value) {
+          if(value.commit === request.commit) {
+            localStorage.setItem(value.commit, web3.toAscii(result[1]));
+            value.orderstate = 2;
+          }
+        });
+      }
+      if(currentUser === request.provider) {
+        $.publish('notice', 'Your response has been sent automatically.');
+      }
+    });
   });
 
   // buy
