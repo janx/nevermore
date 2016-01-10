1b26cbf7987d05cf271af59d1a0821a01559021f
Jan Xie <jan.h.xie@gmail.com>
2016-01-10 08:27:52 +0800

    refacotr new record handling

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -204,6 +204,7 @@ app.controller('SearchCtrl', ['$scope', function ($scope) {
   var source = $("#detail-data-template").html();
   var template = Handlebars.compile(source);
   $scope.showDetailData = function(commit) {
+    console.log('showDetailData', commit);
     var data = localStorage.getItem(commit);
     var dataModal = $('#detail-data-modal')
     if (data) {
@@ -272,7 +273,7 @@ app.controller('CreateCtrl', ['$scope', function ($scope) {
   function makeHash(creditRecord) {
     // TODO: make a better rule for generate hash
     var msg = "" + creditRecord.identity + (new Date()).toString()
-    return SHA256(msg);
+    return '0x' + web3.sha3(msg, {encoding: "hex"});
   }
 
   $scope.categoryOptions = [
@@ -291,6 +292,7 @@ app.controller('CreateCtrl', ['$scope', function ($scope) {
   $scope.createCreditRecord = function() {
     var commit = makeHash(creditRecord)
 
+    console.log(commit);
     var data = {
       commit: commit,
       identity: creditRecord.identity,
@@ -443,31 +445,41 @@ angular.element(document).ready(function() {
     if(address === result.args.provider) {
       own = true
     }
-    //FIXME: Jan... fuck you!!! where is the commit.
 
-    var book = {
-      identity: decodeFromBytes32(result.args.user),
-      category: result.args.category.toNumber(), // 0: 信用贷款 1: 担保贷款  2: 抵押贷款
-      state: result.args.state.toNumber(), // 0: 申请中 1: 进行中 2: 已完结
-      fee: result.args.fee.toNumber(),
-      timestamp: result.args.timestamp.toString(), // unix timestamp
-      provider: result.args.provider,
-      commit: result.args.commit,
-      orderstate: 0,
-      owner: own
-    };
+    var commit = result.args.commit;
+    credit_book.get(commit).then(function(record) {
+      var provider = record[0];
+      var identity = record[1];
+      var category = record[2];
+      var state = record[3];
+      var fee = record[4];
+      var timestamp = record[5];
 
-    var included = false
-    $.each(window.credit_records, function(index, record) {
-      if(record.commit === book.commit) {
-        included = true
+      var book = {
+        identity: decodeFromBytes32(identity),
+        category: category.toNumber(), // 0: 信用贷款 1: 担保贷款  2: 抵押贷款
+        state: state.toNumber(), // 0: 申请中 1: 进行中 2: 已完结
+        fee: fee.toNumber(),
+        timestamp: timestamp.toString(), // unix timestamp
+        provider: provider,
+        commit: commit,
+        orderstate: 0,
+        owner: own
+      };
+
+      var included = false
+      $.each(window.credit_records, function(index, record) {
+        if(record.commit === book.commit) {
+          included = true
+        }
+      });
+      if(!included) {
+        debugger
+        window.credit_records.push(book);
       }
+      $.publish('CreditBook:create', book);
+    }).catch(function(e) {
     });
-    if(!included) {
-      debugger
-      window.credit_records.push(book);
-    }
-    $.publish('CreditBook:create', book);
   });
 
   // watch request
